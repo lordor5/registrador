66f884e18775fd06a4f4c2a9701307a4da78fee1
Lordor5 <thelordor5@gmail.com>
2024-09-16 11:21:54 +0200

    adding waiter

--- register.js
+++ register.js
@@ -3,13 +3,28 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Wait for the scheduled time
-async function register() {
+async function main() {
   const delay = calculateDelayUntil10AM();
 
   // Delay the execution until 10 AM Spain time
   await new Promise((resolve) => setTimeout(resolve, delay));
 
+  // Load the time from the time.json file
+  const timeConfig = JSON.parse(fs.readFileSync("time.json"));
+  console.log(timeConfig);
+
+  let arr = register(timeConfig.registros);
+
+  while (arr.length !== timeConfig.registros.length) {
+    let arr = await register(timeConfig.registros);
+    console.log(arr);
+
+    await new Promise((resolve) => setTimeout(resolve, 300000));
+  }
+}
+main();
+
+async function register(arr) {
   const browser = await puppeteer.launch({
     headless: true,
   }); // Change to true for headless mode
@@ -32,10 +47,6 @@ async function register() {
     "https://intranet.upv.es/pls/soalu/sic_depact.HSemActividades?p_campus=V&p_tipoact=6799&p_codacti=21549&p_vista=intranet&p_idioma=c&p_solo_matricula_sn=&p_anc=filtro_actividad"
   );
 
-  // Load the time from the time.json file
-  const timeConfig = JSON.parse(fs.readFileSync("time.json"));
-  console.log(timeConfig);
-
   // Listen for console messages from the browser context
   page.on("console", (msg) => {
     // Output browser's console messages to Node.js console
@@ -43,11 +54,11 @@ async function register() {
   });
 
   // Click on the available slot (adjust the selector for the specific time you want)
-  await page.evaluate(async (timeConfig) => {
+  newArr = await page.evaluate(async (arr) => {
     // Helper function to create a delay
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    for (const registro of timeConfig.registros) {
+    let newArr = [];
+    for (const registro of arr) {
       // Find the anchor tag that contains 'MUS068' in its text
       const link = Array.from(document.querySelectorAll("a")).find((a) =>
         a.innerText.includes(registro)
@@ -55,17 +66,20 @@ async function register() {
 
       // If the link is found, click on it
       if (link) {
-        //link.click();
+        link.click();
         console.log("registrado en: ", registro);
+        newArr.push(registro);
         await delay(500);
       }
     }
-  }, timeConfig);
+    return newArr;
+  }, arr);
 
   // Close the browser after actions
   await browser.close();
+
+  return newArr;
 }
-register();
 
 // Helper function to calculate delay until 10 AM in Spain (CET)
 function calculateDelayUntil10AM() {
@@ -82,12 +96,13 @@ function calculateDelayUntil10AM() {
   const targetTime = new Date(now);
   targetTime.setHours(10, 0, 0, 0); // 10:00 AM
 
+  let delay = targetTime - now;
+
   // If it's already past 10:00 AM today, set the target to 10:00 AM tomorrow
   if (now > targetTime) {
-    targetTime.setDate(targetTime.getDate() + 1); // Set to the next day
+    delay = 0; // Set to the next day
   }
 
-  const delay = targetTime - now;
   console.log(`Waiting ${delay / 1000 / 60} minutes until 10 AM Spain time...`);
   return delay;
 }
